d980ca477b3e8ec59caccb21169f9e5057166cca
jjmanikan <justinemanikx@gmail.com>
2021-11-15 09:42:30 -0500

    checkout merge

--- WinnerWinnerChickenDinner/Properties/Settings.Designer.cs
+++ WinnerWinnerChickenDinner/Properties/Settings.Designer.cs
@@ -13,7 +13,7 @@ namespace WinnerWinnerChickenDinner.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -100,7 +100,7 @@ namespace WinnerWinnerChickenDinner.Properties
                 this["ContestName"] = value;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
