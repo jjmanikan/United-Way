4b74534d3eb06f9abe379c197e04cad7077c077d
Sasha <amakavchik7794@conestogac.on.ca>
2021-11-29 19:42:55 -0500

    Got rid of dialogue window for adding prizes; now prizes are typed into text box and added with a button. Added some error checking to prevent adding empty prize. Added remove button to each prize

--- WinnerWinnerChickenDinner/SettingsWindow.xaml.cs
+++ WinnerWinnerChickenDinner/SettingsWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Runtime.InteropServices;
 using System.Diagnostics;
 
 
+
+
 namespace WinnerWinnerChickenDinner
 {
 
@@ -35,13 +37,17 @@ namespace WinnerWinnerChickenDinner
 
         private void AddPrize(object sender, RoutedEventArgs e)
         {
-            ShowDialog("Name new Prize:", "NEW PRIZE");
+            string t = prizeBox.Text;
+            AddNewPrize(t);
+            prizeBox.Text = "Enter Prize Here...";
         }
 
         public void AddNewPrize(string prize)
         {
-            if (prize != "")
+            if ((prize != "") && (prize != "Enter Prize Here..."))
             {
+                errorMessage2.Content = "";
+                errorMessage1.Content = "";
                 MainWindow.prizeList.Add(new PrizeBoardItem { PrizeName = prize, Winner = "" });
 
                 prizeBoard.Items.Clear();
@@ -51,57 +57,13 @@ namespace WinnerWinnerChickenDinner
                     prizeBoard.Items.Add(prizeItem);
                 }
             }
+
+            else
+            {
+                errorMessage2.Content = "Please specify the Prize Name";
+                errorMessage1.Content = "";
+            }
         }
-
-
-        public string ShowDialog(string text, string caption)
-        {
-            Form prompt = new Form()
-            {
-                Width = 300,
-                Height = 150,
-                BackColor = System.Drawing.Color.MistyRose,
-                FormBorderStyle = FormBorderStyle.FixedDialog,
-                Text = caption,
-                StartPosition = FormStartPosition.CenterParent
-            };
-
-            System.Windows.Forms.Label txtLabel = new System.Windows.Forms.Label() { Left = 50, Top = 20, Text = text };
-            System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox() { Left = 50, Top = 50, Width = 200 };
-            System.Windows.Forms.Button button = new System.Windows.Forms.Button() { Text = "Add and Go Back", Left = 50, Width = 110, Top = 70, DialogResult = System.Windows.Forms.DialogResult.OK };
-            System.Windows.Forms.Button add = new System.Windows.Forms.Button()
-
-            {
-                Text = "+",
-                Width = 50,
-                Top = 70,
-                Left = 160
-            };
-
-            add.Font = new System.Drawing.Font(button.Font.FontFamily, 20);
-            button.Click += (sender, e) =>
-            {
-                string t = textBox.Text;
-                AddNewPrize(t);
-                prompt.Close();
-            };
-            add.Click += (sender, e) =>
-            {
-                string t = textBox.Text;
-                AddNewPrize(t);
-                textBox.Text = "";
-
-            };
-            prompt.Controls.Add(textBox);
-            prompt.Controls.Add(button);
-            prompt.Controls.Add(add);
-            prompt.Controls.Add(txtLabel);
-            prompt.AcceptButton = button;
-
-            return prompt.ShowDialog() == System.Windows.Forms.DialogResult.OK ? textBox.Text : "";
-
-        }
-
 
         private void btnUploadFile_Click(object sender, RoutedEventArgs e)
         {
@@ -246,7 +208,7 @@ namespace WinnerWinnerChickenDinner
             {
                 errorMessage1.Foreground = Brushes.Red;
                 errorMessage1.Content = "Could not Save";
-                errorMessage2.Content = " -    Please fill out the Required Fields";
+                errorMessage2.Content = "Please fill out the Required Fields";
 
                 if (contestName.Text == "")
                 {
@@ -355,6 +317,7 @@ namespace WinnerWinnerChickenDinner
 
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
+
             MainWindow.prizeList.RemoveAt(prizeBoard.SelectedIndex);
             prizeBoard.Items.RemoveAt(prizeBoard.SelectedIndex);
         }
@@ -380,6 +343,23 @@ namespace WinnerWinnerChickenDinner
         {
             String htmlPath = Directory.GetCurrentDirectory() + @"\..\..\Assets\helpPage.html";
             Process.Start(htmlPath);
+        }
+
+        //when the user clicks in the prize textbox, the "placeholder" is removed
+        private void RemoveText(object sender, RoutedEventArgs e)
+        {
+            if (prizeBox.Text == "Enter Prize Here...")
+            {
+                prizeBox.Text = "";
+            }
+        }
+
+        private void prizeBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(prizeBox.Text))
+            {
+                prizeBox.Text = "Enter Prize Here...";
+            }
         }
     }
 
